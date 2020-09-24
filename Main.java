c8fb7b40ccf80382d95304e2bd725c09c16bcc13
ZocketAlan <33386479+ZocketAlan@users.noreply.github.com>
2020-09-24 18:39:35 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+package ExamenU1;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        FiguraTipoLogoDVD app = new FiguraTipoLogoDVD();
+        app.Draw();
+    }
+}
